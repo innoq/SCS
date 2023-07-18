bc8ebbe6d3868951b0f6e7fd593db3395657cd84
Martin Weck <martin.weck@innoq.com>
2023-07-18 12:27:06 +0200

    adds jump-to-next-section button

--- js/scroll.js
+++ js/scroll.js
@@ -1,9 +1,37 @@
-const backToTopButton = document.getElementById('to-top');
-backToTopButton.style.display = 'none';
-window.addEventListener('scroll', () => {
+/** jump back to top button */
+const toTopButton = document.getElementById('to-top');
+toTopButton.style.display = 'none';
+
+function toggleToTopButton() {
   if (window.pageYOffset > 0) {
-    backToTopButton.style.display = 'block';
+    toTopButton.style.display = 'block';
   } else {
-    backToTopButton.style.display = 'none';
+    toTopButton.style.display = 'none';
   }
+}
+
+/** jump to next section button */
+const sections = document.querySelectorAll('section');
+const jumpToNextSectionButton = document.getElementById('jump-to-next-section');
+updateJumpToNextSectionButtonTarget();
+
+function updateJumpToNextSectionButtonTarget() {
+    const windowHeight = window.innerHeight;
+    const scrollPosition = window.scrollY + windowHeight;
+
+    for (const section of sections) {
+        if (section.offsetTop > scrollPosition && section.id) {
+            jumpToNextSectionButton.href = `#${section.id}`;
+            jumpToNextSectionButton.style.display = 'block';
+            return;
+        }
+    }
+    jumpToNextSectionButton.style.display = 'none';
+}
+
+/** update the buttons when scrolling */
+window.addEventListener('scroll', () => {
+  toggleToTopButton();
+  updateJumpToNextSectionButtonTarget();
 });
+
